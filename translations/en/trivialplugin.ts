70e285b672d8612e5ceacc6482c8275f17cfee42
Sergey Potapov <potapov.s.a@gmail.com>
2012-07-14 14:25:11 +0400

    Добавлена английская локализация для удобства перевода в Transifex.

---
+++ translations/en/trivialplugin.ts
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" sourcelanguage="en">
+<context>
+    <name>TrivialPlugin</name>
+    <message>
+        <source>Trivial Plugin</source>
+        <translation>Trivial Plugin</translation>
+    </message>
+    <message>
+        <source>Example of Vacuum-IM extension plugin</source>
+        <translation>Example of Vacuum-IM extension plugin</translation>
+    </message>
+    <message>
+        <source>Trivial Message</source>
+        <translation>Trivial Message</translation>
+    </message>
+    <message>
+        <source>Use a Vacuum-IM every day, and your hair will be soft and silky!</source>
+        <translation>Use a Vacuum-IM every day, and your hair will be soft and silky!</translation>
+    </message>
+</context>
+</TS>
